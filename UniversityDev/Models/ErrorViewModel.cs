e4ea65098dfbe6a243403235f6e87219520aae1e
kamil_mikolajczyk <kamilmikolajczyk1992@gmail.com>
2026-01-08 01:02:16 +0100

    start

---
+++ UniversityDev/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace UniversityDev.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
